7d1a1e1cd06b332391125bdca2d9eb3f2300a8d3
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-01-09 08:44:02 +0600

    new email and button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
-import StatefulForm from './components/StatefulForm/StatefulForm'
-import SimpleForm from './SimpleForm/SimpleForm'
+import RefForm from './components/RefForm/RefForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
+// import SimpleForm from './SimpleForm/SimpleForm'
 
 function App() {
   
@@ -10,7 +11,8 @@ function App() {
       
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
       
     </>
   )
--- src/components/StatefulForm/StatefulForm.jsx
+++ src/components/StatefulForm/StatefulForm.jsx
@@ -1,30 +1,54 @@
 import { useState } from "react";
 
 
+
 const StatefulForm = () => {
+    const [name, setName]= useState('M Rakib Ahmed');
     const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState('')
     const handleSubmit = e => {
-    //    console.log(e);
-       
-    }
-    
-    const handleEmailChange = e =>{
-console.log(e.target.value);
-setEmail(e.target.value)
+        e.preventDefault();
+        if(password.length < 6){
+          setError('Password must be 6 characters or longer')  
+        }
+        else{
+            setError('')
+        }
+
 
     }
+
+    const handleEmailChange = e => {
+        
+        setEmail(e.target.value)
+
+    }
+
+    const handleNameChange = e => {
+        setName(e.target.value)
+    }
+
+    const handlePasswordChange = e =>{
+      
+      setPassword(e.target.value)
+      
+    }
+    console.log(name, email, password);
     return (
         <div>
             <form onSubmit={handleSubmit}>
 
-                <input type="text" name="name" /> <br />
+                <input value={name} onChange={handleNameChange} type="text" name="name" /> <br />
                 <input onChange={handleEmailChange} type="email" name="email" /> <br />
-                <input type="password" name="password" />
+                <input onChange={handlePasswordChange} type="password" name="password" required/>
                 <br />
                 <button>Submit</button>
 
-
-            </form> 
+              {
+                error && <p>{error}</p>
+              }
+            </form>
         </div>
     );
 };
